b6cbd14be7670ec18235cb592fe27105fda7ec99
David <davidtruhlarac@gmail.com>
2026-03-23 11:54:45 +0100

    Add mongodb compose file

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -11,10 +11,24 @@ $ProjectRoot = "${PSScriptRoot}/.."
 
 $env:AMBULANCE_API_ENVIRONMENT="Development"
 $env:AMBULANCE_API_PORT="8080"
+$env:AMBULANCE_API_MONGODB_USERNAME="root"
+$env:AMBULANCE_API_MONGODB_PASSWORD="heslo"
+
+function mongo {
+    docker compose --file ${ProjectRoot}/deployments/docker-compose/compose.yaml $args
+}
 
 switch ($command) {
     "start" {
-        go run ${ProjectRoot}/cmd/ambulance-api-service
+        try {
+            mongo up --detach
+            go run ${ProjectRoot}/cmd/ambulance-api-service
+        } finally {
+            mongo down
+        }
+    }
+    "mongo" {
+        mongo up
     }
     "openapi" {
         docker run --rm -ti -v ${ProjectRoot}:/local openapitools/openapi-generator-cli generate -c /local/scripts/generator-cfg.yaml
